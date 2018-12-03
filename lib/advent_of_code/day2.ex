8cbc07e0af53f0c205bc57cf2bc3f521b59ad234
nico <nico.piderman@gmail.com>
2018-12-03 13:13:02 -0500

    day 2, first go

---
+++ lib/advent_of_code/day2.ex
@@ -0,0 +1,62 @@
+defmodule AdventOfCode.Day2 do
+
+  @doc "Part one"
+  def checksum(input) do
+    {double_count, triple_count} =
+      Enum.reduce(input, {0, 0}, fn label, {double_count, triple_count} ->
+        counts = char_count(label)
+        doubles = if Enum.any?(counts, fn {_, v} -> v == 2 end), do: 1, else: 0
+        triples = if Enum.any?(counts, fn {_, v} -> v == 3 end), do: 1, else: 0
+
+        {double_count + doubles, triple_count + triples}
+      end)
+
+    double_count * triple_count
+  end
+
+  defp char_count(string) do
+    string
+    |> String.graphemes()
+    |> Enum.reduce(%{}, fn char, acc -> 
+      if Map.has_key?(acc, char) do
+        Map.put(acc, char, acc[char] + 1)
+      else
+        Map.put(acc, char, 1)
+      end
+    end)
+  end
+
+  def common_letters(input) do
+    Enum.reduce_while(input, MapSet.new([]), fn label, seen ->
+      permutations = label_permutations(label)
+
+      Enum.reduce_while(permutations, nil, fn permutation, _ ->
+        if MapSet.member?(seen, permutation) do
+          {:halt, permutation}
+        else
+          {:cont, nil}
+        end
+      end)
+      |> case do
+        nil -> {:cont, update_seen(seen, permutations)} 
+        found -> {:halt, found}
+      end
+    end)
+  end
+
+  defp label_permutations(label) do
+    chars = String.graphemes(label)
+    length = Enum.count(chars)
+
+    for i <- 0..length-1 do
+      {start, [_ | rest]} = Enum.split(chars, i) 
+      Enum.join(start ++ rest, "")
+    end
+  end
+
+  defp update_seen(mapset, new) do
+    Enum.reduce(new, mapset, fn i, acc ->
+      MapSet.put(acc, i)
+    end)
+  end
+end
---
+++ test/advent_of_code/day2_test.exs
@@ -0,0 +1,52 @@
+defmodule AdventOfCode.Day2Test do
+  use ExUnit.Case, async: true
+  alias AdventOfCode.Day2
+
+  @input [:code.priv_dir(:advent_of_code), "inputs", "day2.txt"] 
+    |> Path.join()
+    |> File.read!()
+    |> String.trim()
+    |> String.split("\n")
+
+  describe "part one - checksum" do
+    test "example" do
+      input = [
+        "abcdef",
+        "bababc",
+        "abbcde",
+        "abcccd",
+        "aabcdd",
+        "abcdee",
+        "ababab"
+      ]
+
+      assert Day2.checksum(input) == 12
+    end
+
+    test "problem" do
+      assert Day2.checksum(@input) == 6225
+    end
+  end
+
+  describe "part two" do
+    test "example" do
+      input =
+        """
+        abcde
+        fghij
+        klmno
+        pqrst
+        fguij
+        axcye
+        wvxyz
+        """
+        |> String.split("\n")
+      
+      assert Day2.common_letters(input) == "fgij"
+    end
+
+    test "problem" do
+      assert Day2.common_letters(@input) == "revtaubfniyhsgxdoajwkqilp"
+    end
+  end
+end
