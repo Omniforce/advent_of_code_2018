22d1a52fd8fb92428275ebc56728455c69adb2f0
nico <nico.piderman@gmail.com>
2018-12-05 20:00:03 -0500

    day 5!

---
+++ lib/advent_of_code/day5.ex
@@ -0,0 +1,41 @@
+defmodule AdventOfCode.Day5 do
+
+  def react(chars) do
+    chars
+    |> Enum.reduce([], &do_reaction/2)
+    |> Enum.reverse()
+    |> List.to_string()
+  end
+
+  defp do_reaction(char, []), do: [char]
+  defp do_reaction(char, [next | rest] = acc) do
+    if polar_opposites?(char, next) do
+      rest
+    else
+      [char | acc]
+    end
+  end
+
+  def polar_opposites?(a, a), do: false
+  def polar_opposites?(<<left::utf8>>, <<right::utf8>>) do
+    abs(left - right) == 32
+  end
+
+  def improved_polymer(chars) do
+    chars
+    |> Enum.map(&String.downcase/1)
+    |> Enum.uniq()
+    |> Enum.reduce(%{}, fn remove, acc ->
+      length =
+        chars
+        |> Enum.reject(fn char ->
+          char == remove || char == String.upcase(remove)
+        end)
+        |> react()
+        |> String.length()
+      Map.put(acc, remove, length)
+    end)
+    |> Enum.min_by(fn {_, v} -> v end)
+  end
+
+end
---
+++ test/advent_of_code/day5_test.exs
@@ -0,0 +1,42 @@
+defmodule AdventOfCode.Day5Test do
+  use ExUnit.Case, async: true
+  alias AdventOfCode.Day5
+
+  @input [:code.priv_dir(:advent_of_code), "inputs", "day5.txt"]
+         |> Path.join()
+         |> File.read!()
+         |> String.trim()
+         |> String.split("", trim: true)
+        
+
+  describe "part one" do
+    test "react/1" do
+      input = "dabAcCaCBAcCcaDA" |> String.split("", trim: true)
+      assert "dabCBAcaDA" = Day5.react(input)
+    end
+
+    test "polar_opposites?/2" do
+      assert Day5.polar_opposites?("a", "A")
+      assert Day5.polar_opposites?("Z", "z")
+      assert Day5.polar_opposites?("n", "N")
+      refute Day5.polar_opposites?("a", "B")
+      refute Day5.polar_opposites?("a", "a")
+      refute Day5.polar_opposites?("U", "U")
+    end
+
+    test "problem" do
+      assert 10878 == Day5.react(@input) |> String.length()
+    end
+  end
+
+  describe "part two" do
+    test "improved_polymer/1" do
+      input = "dabAcCaCBAcCcaDA" |> String.split("", trim: true)
+      assert {"c", 4} = Day5.improved_polymer(input)
+    end
+
+    test "problem" do
+      assert {"f", 6874} == Day5.improved_polymer(@input)
+    end
+  end
+end
